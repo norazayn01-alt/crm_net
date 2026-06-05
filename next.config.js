20e40fcb8189574b2b5051e528ee16f5e05fbb88
alien <aliendevuz@gmail.com>
2026-06-05 06:05:19 +0500

    CI/CD applied

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // O'z IP manzilingizni yoki domainingizni qo'shing
-  allowedDevOrigins: ['*'], 
+  allowedDevOrigins: ['*'],
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
