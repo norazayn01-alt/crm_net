aa0d89262f9bd248d7d3b8a72bb721e76dabe6ca
alien <aliendevuz@gmail.com>
2026-06-05 05:40:33 +0500

    update

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // O'z IP manzilingizni yoki domainingizni qo'shing
+  allowedDevOrigins: ['*'], 
+};
+
+module.exports = nextConfig;
